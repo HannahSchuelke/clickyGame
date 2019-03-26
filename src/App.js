a077a5ebb2e924942b6f864dca4f3a3a04b9b69b
Hannah Schuelke <hannah.r.schuelke@gmail.com>
2019-03-26 09:28:40 -0500

    Updates

--- src/App.js
+++
@@ -1,8 +0,0 @@
-// import React, { Component } from 'react';
-import React from "react";
-// import friends from "./icons.json"
-import Game from './components/Game';
-
-const App = () => <Game />
-
-export default App;
--- src/components/Container/Container.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Container = props => <main className="container">{props.children}</main>;
-
-export default Container;
