5fd49f8cc8cb7773ab5415f876b359b591140768
Hannah Schuelke <hannah.r.schuelke@gmail.com>
2019-03-25 19:43:18 -0500

    kittyclick merge

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,14 @@
+import React,{ Component} from "react";
+
+class Nav extends Component{
+    render(){
+        return(
+            <div>
+                <Nav></Nav>
+                <Header></Header>
+                <Container></Container>
+                <Footer></Footer>
+            </div>
+        )
+    }
+}
