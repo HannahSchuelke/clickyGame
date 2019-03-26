68b38d62c36485be99683ea1fbeaa1ed1c4374ba
Hannah Schuelke <hannah.r.schuelke@gmail.com>
2019-03-26 09:19:44 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/HannahSchuelke/clickygame/precache-manifest.dc09bfb7073e1f571da69951d9b69054.js"
+  "/clickygame/precache-manifest.578345ddf0778b89dab86602caee7f6f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/HannahSchuelke/clickygame/index.html", {
+workbox.routing.registerNavigationRoute("/clickygame/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
