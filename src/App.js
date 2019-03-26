ccbe4d467161f071b8fef65517ab5bfb8a954a38
Hannah Schuelke <hannah.r.schuelke@gmail.com>
2019-03-25 20:35:39 -0500

    debugging

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React,{ Component} from "react";
+import Game from './components/Game';
 // import logo from './logo.svg';
 // import './App.css';
 
