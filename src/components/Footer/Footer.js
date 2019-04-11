14ce106a8c4f90b1a90bec3a7501138e49c9165c
Hannah Schuelke <hannah.r.schuelke@gmail.com>
2019-04-11 15:51:16 -0500

    depoy cleaned and styled

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = props => (
+  <footer className="footer">
+    <h5>Hannah Schuelke</h5>
+  </footer>
+);
+
+export default Footer;
