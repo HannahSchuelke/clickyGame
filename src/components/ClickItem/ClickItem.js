b1034e271c18e04b99391f20c069fe5fe6000fb3
Hannah Schuelke <hannah.r.schuelke@gmail.com>
2019-04-11 21:26:46 -0500

    trying to deploy with styling 2

--- src/components/ClickItem/ClickItem.js
+++ src/components/ClickItem/ClickItem.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./ClickItem.css";
 
-
 const ClickItem = props => (
     <div className="icons" onClick={() => props.handleClick(props.id)}>
         <div className="Container">
