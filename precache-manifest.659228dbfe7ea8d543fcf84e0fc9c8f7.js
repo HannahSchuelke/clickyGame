04c65b83cab9d038ed34e3709ce92c33b168ae11
Hannah Schuelke <hannah.r.schuelke@gmail.com>
2019-04-11 22:17:03 -0500

    Updates

---
+++ precache-manifest.659228dbfe7ea8d543fcf84e0fc9c8f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "361ec270429ae3d6a819",
+    "url": "/clickygame/static/js/main.3f2f11c4.chunk.js"
+  },
+  {
+    "revision": "07f9086e349abed10ef1",
+    "url": "/clickygame/static/js/2.9d306826.chunk.js"
+  },
+  {
+    "revision": "361ec270429ae3d6a819",
+    "url": "/clickygame/static/css/main.e1bf9e1f.chunk.css"
+  },
+  {
+    "revision": "0e10a811ad3f8294fa92813ce4a6f34e",
+    "url": "/clickygame/index.html"
+  }
+];
